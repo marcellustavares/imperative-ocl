701405c41582bc624b9c37a0d815d6ee8bb451fc
marcellustavares <marcellustavares@gmail.com>
2010-03-20 20:40:04 +0000

    Junit testing (PrimitiveLiteralExp)

---
+++ test/OCLExpressionTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestSuite;
+
+public class OCLExpressionTest {
+
+    public static void main(String[] args) throws Exception {
+
+        TestSuite suite = new TestSuite();
+
+        suite.addTestSuite(TestPrimitiveLiteralExp.class);
+
+        junit.textui.TestRunner.run(suite);
+    }
+
+}
---
+++ test/TestPrimitiveLiteralExp.java
@@ -0,0 +1,49 @@
+import junit.framework.TestCase;
+
+import org.orcas.iocl.cst.BooleanLiteralExpCS;
+import org.orcas.iocl.cst.IOCLExpressionCS;
+import org.orcas.iocl.cst.IntegerLiteralExpCS;
+import org.orcas.iocl.cst.RealLiteralExpCS;
+import org.orcas.iocl.cst.StringLiteralExpCS;
+import org.orcas.iocl.exception.IOCLException;
+import org.orcas.iocl.parser.IOCLParserUtil;
+
+public class TestPrimitiveLiteralExp extends TestCase {
+
+    public void testBooleanLiteralExp() throws IOCLException {
+        exp = "true";
+        ioclExp = IOCLParserUtil.parse(exp);
+
+        assertTrue(ioclExp instanceof BooleanLiteralExpCS);
+
+        exp = "false";
+        ioclExp = IOCLParserUtil.parse(exp);
+
+        assertTrue(ioclExp instanceof BooleanLiteralExpCS);
+    }
+
+    public void testIntegerLiteralExp() throws IOCLException {
+        exp = "2";
+        ioclExp = IOCLParserUtil.parse(exp);
+
+        assertTrue(ioclExp instanceof IntegerLiteralExpCS);
+    }
+
+    public void testRealLiteralExp() throws IOCLException {
+        exp = "2.2";
+        ioclExp = IOCLParserUtil.parse(exp);
+
+        assertTrue(ioclExp instanceof RealLiteralExpCS);
+    }
+
+    public void testStringLiteralExp() throws IOCLException {
+        exp = "'marcellus'";
+        ioclExp = IOCLParserUtil.parse(exp);
+
+        assertTrue(ioclExp instanceof StringLiteralExpCS);
+    }
+
+    protected String exp;
+    protected IOCLExpressionCS ioclExp;
+
+}

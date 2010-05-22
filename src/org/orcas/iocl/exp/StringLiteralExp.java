5234450b74cd06f4d089a3966d1971a77084f33e
marcellustavares <marcellustavares@gmail.com>
2010-05-22 16:57:28 +0000

    Removed classes in exp package

--- src/org/orcas/iocl/exp/StringLiteralExp.java
+++
@@ -1,9 +0,0 @@
-package org.orcas.iocl.exp;
-
-public interface StringLiteralExp extends PrimitiveLiteralExp {
-
-    public String getStringSymbol();
-
-    public void setStringSymbol(String stringSymbol);
-
-}

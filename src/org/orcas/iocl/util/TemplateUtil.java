b4d1faf32933aded5e381e8f34b882d1648abb0f
Marcellus Tavares <marcellustavares@gmail.com>
2010-05-27 02:07:13 -0300

    Freemaker

---
+++ src/org/orcas/iocl/util/TemplateUtil.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2010  Marcellus C. Tavares
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.orcas.iocl.util;
+
+import java.io.StringWriter;
+import java.util.Map;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+
+public class TemplateUtil {
+
+	public static String process(String templateName, Map<String, String> map) {
+		try {
+			Template template = _instance._configuration.getTemplate(
+				"templates/" + templateName + ".ftl");
+
+			StringWriter writer = new StringWriter();
+			template.process(map, writer);
+			writer.flush();
+
+			return writer.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private TemplateUtil() {
+		_configuration = new Configuration();
+
+		_configuration.setObjectWrapper(new DefaultObjectWrapper());
+		_configuration.setTemplateLoader(
+			new ClassTemplateLoader(TemplateUtil.class, "/"));
+	}
+
+	private static TemplateUtil _instance = new TemplateUtil();
+	private Configuration _configuration;
+
+}

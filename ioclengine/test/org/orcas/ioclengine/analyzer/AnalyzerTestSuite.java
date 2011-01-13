cd17d96cba499af649d7bde3105a845a158f769c
Marcellus Tavares <marcellustavares@gmail.com>
2011-01-13 14:37:13 -0300

    Typo

--- ioclengine/test/org/orcas/ioclengine/analyzer/AnalyzerTestSuite.java
+++ ioclengine/test/org/orcas/ioclengine/analyzer/AnalyzerTestSuite.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.orcas.ioclengine.analayzer;
+package org.orcas.ioclengine.analyzer;
 
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
--- ioclengine/test/org/orcas/ioclengine/analyzer/OperationCallTest.java
+++ ioclengine/test/org/orcas/ioclengine/analyzer/OperationCallTest.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.orcas.ioclengine.analayzer;
+package org.orcas.ioclengine.analyzer;
 
 import org.orcas.ioclengine.IOCLEngine;
 import org.orcas.ioclengine.exception.SemanticException;
